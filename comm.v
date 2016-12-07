fb9976ab94398c8335c9973cdaecd5dc242e2f14
Chenzhuo Zhu <markzhu95@gmail.com>
2016-12-07 05:35:26 -0800

    Add Debounce.

--- comm.v
+++ comm.v
@@ -6,13 +6,21 @@ module comm (
      output wire    [1:0]         out_valid,
      input wire    [7:0]    phr_psdu_in,
      input wire       phr_psdu_in_valid,
-     input wire       clk,
+     input wire       sysclk,
+     input wire       iclk,
      input wire       reset_n
 );
 
 wire framing_encoding_out;
 wire framing_encoding_out_valid;
+wire clk;
  
+debounce u0debounce(
+    .i_clk(sysclk),
+    .i_rst_n(reset_n),
+    .i_key(iclk),
+    .o_key_val(clk)
+)
 
 framing_encoding u0framing_encoding(
     .framing_encoding_out        (framing_encoding_out),
